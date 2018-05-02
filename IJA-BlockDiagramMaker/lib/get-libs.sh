f7f4cdba060d5904158c12c3d5a2734ba5920e05
Harticon <hromadkavojta@gmail.com>
2018-05-02 23:18:42 +0200

    SKRIPT UMIM VIM

---
+++ IJA-BlockDiagramMaker/lib/get-libs.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1T8yjwKITr5D0wQ6jrJtVnBRmyeLH9bqs' -O LUCON.TTF
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=18Up4IhhpTWISmLGXSdNCC_19BXJKVMKM' -O SegoeUI.ttf
